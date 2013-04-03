35df7a8fe022f7cfede6de544c542aee25ae27d7
nuonical <nuonical@gmail.com>
2013-04-03 14:50:38 +0000

    Added WordWrap to code editor (saves to user preferences)

---
+++ DirigoEdge/DirigoEdge/Areas/Admin/Controllers/ContentAdminController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace DirigoEdge.Areas.Admin.Controllers
+{
+    public class ContentAdminController : Controller
+    {
+		[Authorize(Roles = "Administrators")]
+		public JsonResult SetWordWrap(bool wordWrap)
+		{
+			var userName = Membership.GetUser().UserName;
+
+			if (!String.IsNullOrEmpty(userName))
+			{
+				using (var context = new DataContext())
+				{
+					context.Users.FirstOrDefault(x => x.Username == userName).ContentAdminWordWrap = wordWrap;
+
+					context.SaveChanges();
+				}
+			}
+
+			return new JsonResult();
+		}
+    }
+}
