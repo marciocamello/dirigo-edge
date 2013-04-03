ebe9bd46b785197fda5dbcfe69442e85b355b6b8
nuonical@gmail.com <nuonical@gmail.com@794f9bf3-83b0-cf77-3f5d-d283014e4b56>
2013-04-03 20:58:39 +0000

    New captcha method for contact form. Added a stub for contact form sending / smtp logic.

--- trunk/DirigoEdge/DirigoEdge/Controllers/HomeController.cs
+++ trunk/DirigoEdge/DirigoEdge/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
@@ -49,5 +52,78 @@ namespace DirigoEdge.Controllers
 
 			return new XmlSitemapResult(items);
 		}
+
+		#region AjaxControllers
+
+		public JsonResult ContactVerify(string qaptcha_key)
+		{
+			bool error = true;
+			if (!String.IsNullOrEmpty(qaptcha_key))
+			{
+				error = false;
+				Session["qaptcha_key"] = qaptcha_key;
+			}
+
+
+			return new JsonResult() { Data = new { error = error } };
+		}
+
+		public JsonResult ContactFormPost(ContactForm form)
+		{
+			string subject = "New Contact Form Notification";
+			string sendMessageTo = "you@email.com";
+			string messageFrom = "no-reply@myaddress.com";
+			string smtpCredential = "mail.domain.com";
+			string smtpCredentialLogin = "noreply@mydomain.com";
+			string smtpPassword = "myPassword";
+
+			var result = new JsonResult() { JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+
+			// Check Captcha
+			if (Session["qaptcha_key"] != null)
+			{
+				var sb = new StringBuilder();
+
+				sb.Append("Name : " + form.name + "<br />");
+				sb.Append("Contact Email : " + form.contactEmail + "<br />");
+				sb.Append("Subject : " + form.subject + "<br /><br />");
+				sb.Append("Comments : " + form.message + "<br />");
+
+				// Send Form Data
+				try
+				{
+					var msg = new MailMessage();
+					msg.Body = sb.ToString();
+					msg.IsBodyHtml = true;
+					msg.From = new MailAddress(messageFrom);
+					msg.Subject = subject;
+					msg.To.Add(sendMessageTo);
+
+					var smtp = new SmtpClient(smtpCredential);
+					smtp.Credentials = new NetworkCredential(smtpCredentialLogin, smtpPassword);
+					smtp.Port = 25;
+					smtp.Send(msg);
+				}
+				catch (Exception e)
+				{
+
+				}
+			}
+
+			return result;
+		}
+
+		#endregion
+
+		public class ContactForm
+		{
+			public string message { get; set; }
+			public string contactEmail { get; set; }
+			public string name { get; set; }
+			public string subject { get; set; }
+
+			public string captchaId { get; set; }
+		}
+
 	}
 }
