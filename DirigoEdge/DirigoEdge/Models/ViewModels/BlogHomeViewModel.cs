d2e294231d27dccac399f8f96bfe6a49c845d3ed
nuonical <nuonical@gmail.com>
2013-04-04 13:42:08 +0000

    Contact Form Enhancements. Fixed OG Type in Homepage. Added Blog Category Count.

--- DirigoEdge/DirigoEdge/Models/ViewModels/BlogHomeViewModel.cs
+++ DirigoEdge/DirigoEdge/Models/ViewModels/BlogHomeViewModel.cs
@@ -11,16 +11,16 @@ namespace DirigoEdge.Models.ViewModels
 		public int MaxBlogCount;
 
 		public Blog FeaturedBlog;
-		public List<BlogCategory> Categories;
+		public List<BlogCatExtraData> Categories;
 		public bool ReachedMaxBlogs;
 
 		public BlogHomeViewModel()
 		{
-			using (DataContext context = new DataContext())
+			using (var context = new DataContext())
 			{
 				// Get the amount of blogs to load on homepage
 				var blogSettings = context.BlogSettings.FirstOrDefault();
-				if (blogSettings != null)
+				if (blogSettings == null)
 				{
 					MaxBlogCount = blogSettings.MaxBlogsOnHomepageBeforeLoad;
 				}
@@ -35,11 +35,24 @@ namespace DirigoEdge.Models.ViewModels
 				// Used to show the "Load More" link at the bottom to ajax in more content
 				ReachedMaxBlogs = context.Blogs.Count() > MaxBlogCount;
 
-				Blogs = context.Blogs.Where(x => x.IsActive == true).OrderByDescending(blog => blog.Date).Take(MaxBlogCount).ToList();
-				FeaturedBlog = context.Blogs.FirstOrDefault(x => x.IsFeatured == true && x.IsActive == true);
+				Blogs = context.Blogs.Where(x => x.IsActive == true).Take(MaxBlogCount).ToList();
+				FeaturedBlog = context.Blogs.FirstOrDefault(x => x.IsFeatured == true);
+				Categories = new List<BlogCatExtraData>();
 
-				Categories = context.BlogCategories.Where(x => x.IsActive == true).ToList();
+				var cats = context.BlogCategories.Where(x => x.IsActive == true).ToList();
+				foreach (var cat in cats)
+				{
+					int count = context.Blogs.Count(x => x.MainCategory == cat.CategoryName);
+					Categories.Add(new BlogCatExtraData() { TheCategory = cat, BlogCount = count });
+				}
+
 			}
 		}
+	}
+
+	public class BlogCatExtraData
+	{
+		public BlogCategory TheCategory;
+		public int BlogCount;
 	}
 }
