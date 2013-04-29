9be9468e4bbd19cc2ae39b38aa00be8d9c75ef34
nuonical <nuonical@gmail.com>
2013-04-29 22:05:13 +0000

    Content Templates now pull from directory instead of having to add them to a collection. Very cool - thanks Scott P!

--- DirigoEdge/DirigoEdge/Models/DataModels/ContentTemplates.cs
+++ DirigoEdge/DirigoEdge/Models/DataModels/ContentTemplates.cs
@@ -7,18 +7,40 @@ namespace DirigoEdge.Models.DataModels
 {
 	public class ContentTemplates
 	{
-		// Here you can define view templates for content pages
-		public Dictionary<string, ContentTemplate> Templates = new Dictionary<string, ContentTemplate>
+		private const string _templatesLocation = "/Views/Content/Templates";
+
+		public Dictionary<string, ContentTemplate> Templates;
+
+		public ContentTemplates()
 		{
-			// Empty / Default Template
+			addTemplates();
+		}
+
+		private void addTemplates()
+		{
+			Templates = new Dictionary<string, ContentTemplate>();
+
+			// Get list of Template files in this location
+			var templateList = System.IO.Directory.EnumerateFiles(HttpContext.Current.Server.MapPath(string.Format("~{0}", _templatesLocation)));
+
+			// Loop through each template in the folder and add them to the collection
+			foreach (string template in templateList)
 			{
-				"blank", new ContentTemplate
-				{
-					TemplateName = "None",
-					ViewLocation = "/Views/Content/Templates/BlankTemplate.cshtml"
-				}
+				string[] ignorePath = new string[] { @"\Views\Content\Templates\" };
+				string[] ignoreExtenstion = new string[] { ".cshtml" };
+
+				// split parts of file path
+				string[] filename = template.Split(ignorePath, StringSplitOptions.RemoveEmptyEntries);
+				string[] templateName = filename[1].Split(ignoreExtenstion, StringSplitOptions.RemoveEmptyEntries); // includes the word "Template"
+				string[] templateShortName = templateName[0].Split(new string[] { "Template" }, StringSplitOptions.RemoveEmptyEntries); // stripped "Template"
+
+				// add the template to the list
+				ContentTemplate newTemplate = new ContentTemplate();
+				newTemplate.TemplateName = templateShortName[0];
+				newTemplate.ViewLocation = string.Format("{0}/{1}", _templatesLocation, filename[1]);
+				Templates.Add(newTemplate.TemplateName.ToLower(), newTemplate);
 			}
-		};
+		}
 	}
 
 	public class ContentTemplate
